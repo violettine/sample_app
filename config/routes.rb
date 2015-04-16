164c7339e2334fc96a4fa2a84fb052ade30f0e26
violettine <violettine@freenet.de>
2015-04-16 18:44:40 +0200

    finish layout with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'signup'  => 'users#new'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
