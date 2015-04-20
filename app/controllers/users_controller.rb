f407880044c37129a2c651f2fec8b0fd7d22e0b6
violettine <violettine@freenet.de>
2015-04-20 12:10:52 +0200

    add redirect for correct user_form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      redirect_to @user # = redirect_to user_url(@user) 
     else
       render 'new'
     end
