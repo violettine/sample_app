ead91be2fcd5c352e03ab0b6c4255794dd4253eb
violettine <violettine@freenet.de>
2015-04-20 12:34:14 +0200

    add test for valid signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,4 +12,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test "valid signup Information" do 
+    get signup_path
+    assert_difference 'User.count', 1 do
+    post_via_redirect users_path, user: { name:  "Test Person",
+                               email: "user@valid.com",
+                               password:              "foobar",
+                               password_confirmation: "foobar" }
+    end
+    assert_template 'users/show'
+  end
 end
