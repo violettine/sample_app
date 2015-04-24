33392fc5db670c836badf81f1793017365bb8fd3
violettine <violettine@freenet.de>
2015-04-24 10:22:39 +0200

    password resets controller and form

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,6 +48,18 @@ class User < ActiveRecord::Base
     UserMailer.account_activation(self).deliver_now
   end   
 
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
   private
 
     # Converts email to all lower-case.
