72ad60e11144fd1fce327a895197ca4374db6f15
violettine <violettine@freenet.de>
2015-04-27 13:00:32 +0200

    bug fix in db file

--- db/migrate/20150424114505_create_microposts.rb
+++ db/migrate/20150424114505_create_microposts.rb
@@ -6,6 +6,6 @@ class CreateMicroposts < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    add_foreign_key :microposts, [:user_id, :created_at]
+    add_index :microposts, [:user_id, :created_at]
   end
 end
